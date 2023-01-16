318ca61cfda53dc493f4de9ab59147fa1a36897a
Fabio Ticconi <fabio.ticconi@gmail.com>
2023-01-16 09:10:05 +0000

    version 0.9

---
+++ R/autoGO-package.R
@@ -0,0 +1,28 @@
+#' @keywords internal
+#' @name autoGO
+#' @docType package
+#' @aliases autoGO-package
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import readr
+#' @import reshape2
+#' @import DESeq2
+#' @import ComplexHeatmap
+#' @import enrichR
+#' @import GSVA
+#' @import utils
+#' @import ggplot2
+#' @import stringr
+#' @import SummarizedExperiment
+#' @import ape
+#' @import dichromat
+#' @import grid
+#' @import imguR
+#' @import ggrepel
+#' @import grDevices
+#' @import dplyr
+#' @importFrom stats median na.omit p.adjust reorder setNames t.test wilcox.test
+#' @importFrom textshape column_to_rownames
+## usethis namespace: end
+NULL
